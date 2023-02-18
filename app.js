2102b00916d56b3d970525546992bd75ec22a637
ouliana <ouliana.kotik@gmail.com>
2023-02-18 12:04:53 +0300

    refactoring done

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const blogsRoute = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRoute);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
